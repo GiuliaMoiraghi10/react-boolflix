9b611285778f18e06b2ea841e0f1407371a4b9eb
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-12 18:40:58 +0100

    Caricamento immagini bandiere

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../components/header'
+import Movies from '../components/movies'
 
 export default function Home() {
 
     return (
         <>
             <Header />
+            <Movies />
         </>
     )
 }
