1e4afe3773439b26230fb49afe75936465086f0b
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-12 17:19:07 +0100

    Modifico il form per ricerca query e collego evento

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,10 +1,27 @@
+import GlobalContext from "../context/GlobalContext"
+import { useState, useContext, useEffects } from 'react'
+
 export default function SearchBar() {
+
+    // disabilito invio del form con una funzione
+    function handleForm(event) {
+        event.preventDefault()
+        fetchMovies(query)
+    }
+
+    // creo variabile di stato dedicata alla query string per ricerca form
+    const [query, setQuery] = useState('')
+
+    // creo variabile per prendere i film dal context (come per i movies in movies.jsx)
+    const { fetchMovies } = useContext(GlobalContext)
+
     return (
 
         <div className="container">
-            <form action="">
-                <label htmlFor="search">Cerca il titolo che vuoi</label>
+            <form onSubmit={handleForm} action="">
+                <label htmlFor="search">Cerca il titolo</label>
                 <input
+                    onChange={(event) => setQuery(event.target.value)}
                     type="text"
                     id="search"
                     name="search"
