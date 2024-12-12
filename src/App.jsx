d8dbc985d1cc9e243e9ca0194dd0a0d75891b92a
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-12 12:53:44 +0100

    Aggiungo struttura base in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import axios from 'axios'
+import { BASE_URI } from './URIelements/baseURI'
+import { KEY_API } from './URIelements/keyAPI'
+import Home from './pages/Home'
+import GlobalContext from './context/GlobalContext'
 
 function App() {
 
   return (
     <>
+      <GlobalContext.Provider value={{}}>
+        <Home />
+      </GlobalContext.Provider>
     </>
   )
 }
