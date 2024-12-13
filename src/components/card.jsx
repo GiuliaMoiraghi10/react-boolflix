1de3df78123d27b45191dbb3dcc6fa03c3f00dc8
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-13 16:01:52 +0100

    Refactory Card con piccole correzioni

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,47 +1,15 @@
-import det from '../assets/bandiere/det.png'
-import eng from '../assets/bandiere/eng.png'
-import fra from '../assets/bandiere/fra.png'
-import ita from '../assets/bandiere/ita.png'
-import jap from '../assets/bandiere/jap.png'
-import spa from '../assets/bandiere/spa.png'
-import placeholder from '../assets/bandiere/placeholder.png'
-import React from 'react'
-
 export default function Card({ item = {} }) {
 
-    // creo variabile destrutturata per passare tutte le props dell'oggetto (sia dei film che delle serie)
-    const { id, title, name, original_title, original_name, original_language, vote_average } = item
+    // destrutturo oggetto ricevuto da API con elementi che mi servono nelle card
+    const { title, name, original_title, original_name, vote_average, original_language, poster_path } = item
 
-    // creo variabili per distinguere film da serie
-    const movieTitle = title
-    const serieTitle = name
-    const movieOriginalTitle = original_title
-    const serieOriginalTitle = original_name
-
-    // converto immagini
-    const flagLanguage = {
-        det: det,
-        eng: eng,
-        fra: fra,
-        ita: ita,
-        jap: jap,
-        spa: spa,
-    }
-
+    // ritorna quello che voglio stampato nelle card
     return (
-        <>
-            <div>
-                <figure>
-                    {/* <img src={item.image_url} alt="" /> */}
-                </figure>
-                <div>
-                    <h2>{movieTitle || serieTitle}</h2>
-                    <h3>{movieOriginalTitle || serieOriginalTitle}</h3>
-                    <img src={flagLanguage} alt="" />
-                    <p>{original_language}</p>
-                    <p>{vote_average}</p>
-                </div>
-            </div>
-        </>
+        <div>
+            <h3>{title || name}</h3>
+            <h4>{original_title || original_name}</h4>
+            <p>{original_language}</p>
+            <p>{vote_average}</p>
+        </div>
     )
 }
