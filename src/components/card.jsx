260ce85b2009f41d2bc57f874fd3915a49b86ac0
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-15 12:33:10 +0100

    Conversione numero decimale in numero intero da 1 a 5

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -9,20 +9,25 @@ export default function Card({ item = {} }) {
     // destrutturo oggetto ricevuto da API con elementi che mi servono nelle card
     const { title, name, original_title, original_name, vote_average, original_language, poster_path } = item
 
+    // variabile per convertire numero decimale in numero intero da 1 a 5
+    const voteNumberInt = Math.ceil(vote_average / 2)
+
     // ritorna quello che voglio stampato nelle card
     return (
         <div className="col_4">
             <div className="card">
                 <h3>{title || name}</h3>
                 <h4>{original_title || original_name}</h4>
-                <img src={`${BASE_IMG}${SIZE_IMG}${poster_path || placeholder}`} alt="" />
+                {/* <img src={`${BASE_IMG}${SIZE_IMG}${poster_path || placeholder}`} alt="" /> */}
+
+                <img src={poster_path ? `${BASE_IMG}${SIZE_IMG}${poster_path}` : placeholder} alt="" />
 
                 {
                     flagsMap[original_language] ?
                         <img src={flagsMap[original_language]} height={20} alt="" /> :
                         <p>{original_language}</p>
                 }
-                <p>{vote_average}</p>
+                <p>{voteNumberInt}</p>
             </div>
         </div>
     )
